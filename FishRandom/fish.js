aabc4b39b36d1b6603463a1c7a19a98aa24055a6
BeeIsHoney <kyawsuhein4379@gmail.com>
2025-03-04 15:59:02 +0630

    Updated index.html and other files

--- FishRandom/fish.js
+++ FishRandom/fish.js
@@ -1,3 +1,4 @@
+
 function toggleMenu() {
     const navItems = document.getElementById('nav-items');
     if (navItems.style.display === 'flex') {
@@ -7,33 +8,5 @@ function toggleMenu() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("DOM fully loaded and parsed");
 
-    // Retrieve the visit count from local storage
-    let visitCount = localStorage.getItem('visitCount');
-    console.log("Initial visit count from local storage:", visitCount);
 
-    // If visit count is not available, initialize it to 0
-    if (!visitCount) {
-        visitCount = 0;
-    } else {
-        visitCount = parseInt(visitCount, 10); // Convert to integer
-    }
-
-    // Increment the visit count
-    visitCount++;
-    console.log("Updated visit count:", visitCount);
-
-    // Store the updated visit count in local storage
-    localStorage.setItem('visitCount', visitCount);
-
-    // Update the visit count in the HTML
-    const visitCountElement = document.getElementById('visitcount');
-    if (visitCountElement) {
-        visitCountElement.textContent = visitCount;
-        console.log("Visit count updated in HTML");
-    } else {
-        console.error("Element with ID 'visitcount' not found");
-    }
-});
---
+++ Mainpage/actio.js
@@ -0,0 +1,39 @@
+function toggleMenu() {
+    const navItems = document.getElementById('nav-items');
+    if (navItems.style.display === 'flex') {
+        navItems.style.display = 'none';
+    } else {
+        navItems.style.display = 'flex';
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("DOM fully loaded and parsed");
+
+    // Retrieve the visit count from local storage
+    let visitCount = localStorage.getItem('visitCount');
+    console.log("Initial visit count from local storage:", visitCount);
+
+    // If visit count is not available, initialize it to 0
+    if (!visitCount) {
+        visitCount = 0;
+    } else {
+        visitCount = parseInt(visitCount, 10); // Convert to integer
+    }
+
+    // Increment the visit count
+    visitCount++;
+    console.log("Updated visit count:", visitCount);
+
+    // Store the updated visit count in local storage
+    localStorage.setItem('visitCount', visitCount);
+
+    // Update the visit count in the HTML
+    const visitCountElement = document.getElementById('visitcount');
+    if (visitCountElement) {
+        visitCountElement.tex = visitCount;
+        console.log("Visit count updated in HTML");
+    } else {
+        console.error("Element with ID 'visitcount' not found");
+    }
+});
